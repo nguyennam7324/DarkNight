a90ec43e059f7028f520db8a5ad2dc937060375b
Bin2505 <meizum3s0191@gmail.com>
2025-06-18 16:07:06 +0700

    Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float movetoPlayer = 2f;
+    protected Player player;
+    [SerializeField] protected float maxHP = 50f;
+    protected float currentHP;
+    [SerializeField] protected float enterDamage = 10f;
+    [SerializeField] protected float stayDamage = 1f;
+
+    protected virtual void Start()
+    {
+        player = FindAnyObjectByType<Player>();
+        currentHP=maxHP;
+        
+    }
+    protected virtual void Update()
+    {
+        MoveEnemy();
+    }
+    protected void MoveEnemy()
+    {
+        if (player != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, movetoPlayer * Time.deltaTime);
+            FlipEnemy();
+        }
+    }
+    protected void FlipEnemy()
+    {
+        if (player != null)
+        {
+            transform.localScale = new Vector3(player.transform.position.x < transform.position.x ? -1 : 1, 1, 1);
+        }
+    }
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("Máu Enemy hiện tại:" + currentHP);
+        currentHP -= damage;
+        currentHP = Mathf.Max(currentHP, 0);
+        Debug.Log("Máu Enemy hiện tại:" + currentHP);
+
+        
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+
+
+}
